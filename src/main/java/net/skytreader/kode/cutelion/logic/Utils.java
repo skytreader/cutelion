61ba891f4237ef7473f8f291c9e15ed4c54f7e27
Chad Estioco <chadestioco@gmail.com>
2023-07-08 00:29:55 +0200

    Delegate locale validation to a regex.
    Turns out I'm looking for a regex, not a fancy method. For quick checks,
    turns out the best that we can do is check the syntax. A deeper validity
    check involves counter-checking against a massive IANA Language Subtag
    Registry.

--- src/main/java/net/skytreader/kode/cutelion/logic/Utils.java
+++ src/main/java/net/skytreader/kode/cutelion/logic/Utils.java
@@ -1,39 +1,11 @@
 package net.skytreader.kode.cutelion.logic;
 
-import java.util.IllformedLocaleException;
-import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.function.Function;
+import java.util.regex.Pattern;
 
 public class Utils {
 
     public static boolean isValidLocaleString(String l) {
-        String[] parse = l.split("-");
-        if (parse.length == 0 || parse.length >= 4) {
-            return false;
-        } else {
-            Locale.Builder builder = new Locale.Builder();
-            Function<String, Locale.Builder>[] partsProgression = new Function[]{
-                    s -> builder.setLanguage((String) s),
-                    s -> builder.setRegion((String) s),
-                    s -> builder.setVariant((String) s)
-            };
-
-            for (int i = 0; i < parse.length; i++) {
-                try {
-                    partsProgression[i].apply(parse[i]);
-                } catch (IllformedLocaleException ile) {
-                    return false;
-                }
-            }
-
-            Locale loc = builder.build();
-
-            try {
-                return loc.getISO3Language() != null && loc.getISO3Country() != null;
-            } catch (MissingResourceException mre) {
-                return false;
-            }
-        }
+        String trimmed = l.trim();
+        return Pattern.matches("[a-zA-Z]+(-[a-zA-Z0-9]+)*", trimmed);
     }
 }
--- src/test/java/net/skytreader/kode/cutelion/logic/UtilsTest.java
+++ src/test/java/net/skytreader/kode/cutelion/logic/UtilsTest.java
@@ -8,11 +8,25 @@ public class UtilsTest {
 
     @Test
     void testValidLocales() {
-        assertTrue(Utils.isValidLocaleString("en-us"));
+        System.out.println("Valid locales test");
+        String[] testCases = new String[]{
+                "en-us", "en-US", "EN-US", " en-us ", "en"
+        };
+        for (String test : testCases) {
+            System.out.println("Testing '" + test + "'");
+            assertTrue(Utils.isValidLocaleString(test));
+        }
     }
 
     @Test
     void testInvalidLocale() {
-        assertFalse(Utils.isValidLocaleString("en_us"));
+        System.out.println("Invalid locales test");
+        String[] testCases = new String[]{
+                "en_us", "", "-", "en--US", "-US", "en-"
+        };
+        for (String test: testCases) {
+            System.out.println("Testing '" + test + "'");
+            assertFalse(Utils.isValidLocaleString(test));
+        }
     }
 }
