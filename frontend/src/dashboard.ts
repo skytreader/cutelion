91afe097065ee873e940bbf97ed30098c895976b
Chad Estioco <chadestioco@gmail.com>
2023-08-17 17:22:07 +0200

    Refactor some frontend code.

--- frontend/src/dashboard.ts
+++ frontend/src/dashboard.ts
@@ -6,20 +6,18 @@ class Dashboard extends BaseTemplate {
     private projects: Array<Project> = [];
 
     protected override pageContent() {
-        const body = this.projects.length == 0 ? html` <p>No projects found. <button id="start-project">+ Start New Project</button></p> ` : html`
-         <vaadin-horizontal-layout theme="spacing padding" style="justify-content: end">
+        return html`
+        <h2>Project Dashboard</h2>
+        <p ?hidden="${this.projects.length != 0}">No projects found. <vaadin-button theme="primary" id="start-project">+ Start New Project</vaadin-button></p>
+         <vaadin-horizontal-layout theme="spacing padding" style="justify-content: end" ?hidden="${this.projects.length == 0}">
            <vaadin-button theme="primary" id="start-project">+ New</vaadin-button>
            <vaadin-button theme="primary error" id="delete-project">- Delete</vaadin-button>
          </vaadin-horizontal-layout>
-         <vaadin-list-box style="border: 1px solid #edf2f4">
+         <vaadin-list-box style="border: 1px solid #edf2f4" id="project-list">
          ${this.projects.map((project) =>
             html `<vaadin-item><a href="project/edit/${project.id}">${project.name}</a></vaadin-item><hr>`
         )}
         </vaadin-list-box>
-         `;
-        return html`
-        <h2>Project Dashboard</h2>
-        ${body}
         `;
     }
 }
