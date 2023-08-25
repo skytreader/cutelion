4d789080988d1b10e7cf46ab6b1ad8a944b28059
Chad Estioco <chadestioco@gmail.com>
2023-08-25 16:49:03 +0200

    Reformat dashboard frontend to make new project button work again.

--- frontend/src/dashboard.ts
+++ frontend/src/dashboard.ts
@@ -8,14 +8,15 @@ class Dashboard extends BaseTemplate {
     protected override pageContent() {
         return html`
         <h2>Project Dashboard</h2>
-        <p ?hidden="${this.projects.length != 0}">No projects found. <vaadin-button theme="primary" id="start-project">+ Start New Project</vaadin-button></p>
-         <vaadin-horizontal-layout theme="spacing padding" style="justify-content: end" ?hidden="${this.projects.length == 0}">
-           <vaadin-button theme="primary" id="start-project">+ New</vaadin-button>
-           <vaadin-button theme="primary error" id="delete-project">- Delete</vaadin-button>
-         </vaadin-horizontal-layout>
-         <vaadin-list-box style="border: 1px solid #edf2f4" id="project-list">
-         </vaadin-list-box>
-        `;
+        ${ this.projects.length == 0 ?
+            html`<p> No projects found. <vaadin-button theme="primary" id="start-project">+ Start New Project</vaadin-button></p>` :
+            html ` <vaadin-horizontal-layout theme="spacing padding" style="justify-content: end" ?hidden="${this.projects.length == 0}">
+          <vaadin-button theme="primary" id="start-project">+ New</vaadin-button>
+          <vaadin-button theme="primary error" id="delete-project">- Delete</vaadin-button>
+        </vaadin-horizontal-layout>
+        <vaadin-list-box style="border: 1px solid #edf2f4" id="project-list">
+        </vaadin-list-box>
+        `}`;
     }
 }
 
